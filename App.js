8b22182d173e9090fa817c84b32fa57160468980
shavonthadani <shavon.thadani@gmail.com>
2023-03-13 20:51:21 -0400

    Added map screen

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './app/screens/LoginScreen';
 import Login from './app/screens/LoginScreen';
+import MapScreen from './app/screens/MapScreen';
 
 export default function App() {
   return (
-    <LoginScreen />
+    <MapScreen />
   );
 }
 
